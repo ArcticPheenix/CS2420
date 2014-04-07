dfb60ad9c93a8b7a15145cf9bd8b6d160ba1d8eb
ArcticPheenix <phoenix.ckelly@gmail.com>
2014-04-07 13:20:07 -0600

    Grammar changes

--- HashTable/HashTable.h
+++ HashTable/HashTable.h
@@ -1,21 +1,20 @@
-#include <string>
 #ifndef HASHTABLE_CLASS_H
 #define HASHTABLE_CLASS_H
-
+#include <string>
 template <typename T>
 class HashTable
 {
 private:
 	const int SIZE = 26;	// Arbitrary size. Refers to number of letters in English alphabet.
 	int* dataArray;
-	int hash(String key);
-	double retrieve(String key);
+	int hash(string key);
+	double retrieve(string key);
 public:
 	HashTable();
 	~HashTable();
 	void clear();
-	void insert(String key, double value);
-	const double& operator[](String key) const;
+	void insert(string key, double value);
+	const double operator[](string key) const;
 };
 
 template <typename T>
@@ -31,15 +30,19 @@ HashTable<T>::~HashTable()
 }
 
 template <typename T>
-int HashTable<T>::hash(String key)
+int HashTable<T>::hash(string key)
 {
 	//TODO
+	int result = 0;
+	return result;
 }
 
 template <typename T>
-double HashTable<T>::retrieve(String key)
+double HashTable<T>::retrieve(string key)
 {
 	//TODO
+	double result = 0;
+	return result;
 }
 
 template <typename T>
@@ -49,13 +52,13 @@ void HashTable<T>::clear()
 }
 
 template <typename T>
-void HashTable<T>::insert(String key, double value)
+void HashTable<T>::insert(string key, double value)
 {
 	//TODO
 }
 
 template <typename T>
-const double& operator[](String key) const
+const double& operator[](string key) const
 {
 	//TODO
 }
