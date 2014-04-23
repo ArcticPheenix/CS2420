cdf1bc6d7a1080924082a2f994c990ca51cc4261
ArcticPheenix <phoenix.ckelly@gmail.com>
2014-04-23 13:49:12 -0600

    Changed Iterator Class Namespace
    Removed namespace declaration in class

--- Graph/Graph/Iterator.h
+++ Graph/Graph/Iterator.h
@@ -4,27 +4,24 @@
 #define LIST_ITERATOR_H
 #include "Node.h"
 
-namespace list_1
+class Iterator
 {
-	class Iterator
-	{
-    private:
-		Node* n;
-	public:
-		// Constructor
-		Iterator (Node *np);
+private:
+	Node* n;
+public:
+	// Constructor
+	Iterator (Node *np);
         
-		// precondition: is_item is true
-		// post condition n points to the next item in the list
-		void operator++();
+	// precondition: is_item is true
+	// post condition n points to the next item in the list
+	void operator++();
         
-		// precondition: 
-		// postcondition: returns true if there is a valid item
-		bool is_item();
+	// precondition: 
+	// postcondition: returns true if there is a valid item
+	bool is_item();
         
-		// precondition: is_item == true
-		// postcondition returns data that n is pointing at
-		int operator* ();
-	};
-}
+	// precondition: is_item == true
+	// postcondition returns data that n is pointing at
+	int operator* ();
+};
 #endif
